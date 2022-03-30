8a4c7eb264618ff03402208c3a486379fceff5e0
Glitch (giphy-project-aparajita) <none>
2022-03-30 22:48:48 +0000

    🌽🦟 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -13,9 +13,26 @@ $(".search-button").click(function () {
 
     .then(function (body) {
       console.log(body.data);
-    });
+    })
+  
+  
+  
 });
 
 //[ ] Navigate through the API request URL to return only the first gif in the array
 //[ ] HINT: Work your way through the layers one at a time (check your console every time!) to find where the original image URLs are stored
 //[ ] Update your API request so that only the original image URL is logged to the console
+
+$(".search-button").click(function () {
+  fetch(customGiphy)
+  
+    .then(function (response) {
+      return response.json();
+    })
+
+  //not sure if it works
+    .then(function (body) {
+      console.log(body.data.url);
+    })
+  
+});
