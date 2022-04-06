f7867a24bbd8e03287fbaa2a07968151a263b96e
Glitch (giphy-project-aparajita) <none>
2022-04-06 22:22:20 +0000

    💭🏗️ Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,38 +1,22 @@
-//[ ] Declare a variable that stores the following API request URL: https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC
-//[ ] Write the fetch request that logs the entire API request to the console
-
-let customGiphy =
-  "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
-
 $(".search-button").click(function () {
-  fetch(customGiphy)
   
-    .then(function (response) {
-      return response.json();
-    })
+  // Declare a variable that will store the user's input
+  let userInput = $("input").val();
+  console.log(userInput);
 
-    .then(function (body) {
-      console.log(body.data);
-    })
-  
-  
-  
-});
+  // Use string concatenation to include the search term
+  let customGiphy =
+    "https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=pg&api_key=tTVMCPwEb1NapUWHla1pBNt4jKlfEqo1";
+  console.log(customGiphy);
 
-//[ ] Navigate through the API request URL to return only the first gif in the array
-//[ ] HINT: Work your way through the layers one at a time (check your console every time!) to find where the original image URLs are stored
-//[ ] Update your API request so that only the original image URL is logged to the console
+  $(".search-button").click(function () {
+    fetch(customGiphy)
+      .then(function (response) {
+        return response.json();
+      })
 
-$(".search-button").click(function () {
-  fetch(customGiphy)
-  
-    .then(function (response) {
-      return response.json();
-    })
-
-  //not sure if it works
-    .then(function (body) {
-      console.log(body.data.url);
-    })
-  
+      .then(function (body) {
+        console.log(body.data);
+      });
+  });
 });
