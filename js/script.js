04d59ff5c90ca83f19344fd1e27eb3c6fe28009e
Glitch (giphy-project-aparajita) <none>
2022-04-27 22:27:01 +0000

    💾🦵 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 $(".search-button").click(function () {
-  
   // Declare a variable that will store the user's input
   let userInput = $("input").val();
-  console.log(userInput);
 
   // Use string concatenation to include the search term
-  let customGiphy =
-    "https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=pg&api_key=tTVMCPwEb1NapUWHla1pBNt4jKlfEqo1";
-  console.log(customGiphy);
+  let customGiphy = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=pg&api_key=tTVMCPwEb1NapUWHla1pBNt4jKlfEqo1`;
 
-  $(".search-button").click(function () {
-    fetch(customGiphy)
-      .then(function (response) {
-        return response.json();
-      })
+  fetch(customGiphy)
+    .then(function (response) {
+      return response.json();
+    })
 
-      .then(function (body) {
-        console.log(body.data);
-      });
-  });
+    .then(function (data) {
+      let randomNum = Math.floor(Math.random() * data.data.length);
+      let randomGif = data.data[randomNum].images.original.url;
+      console.log(randomGif);
+      $(".display").html(`<img src="${randomGif}"></img>`);
+    });
 });
+
+//[ ] Update the request URL in your fetch request to display a random gif onto the screen
